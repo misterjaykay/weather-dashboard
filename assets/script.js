59d13756e1948d9fe7abf7a9f3c9ff7fb007b295
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-04 16:55:51 -0400

    using different queryURL for 5days forecast

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,30 @@
 
 
 function searchCityWeather(input) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=1a4d9161e2e475ad0fc5e0df86649984";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=1a4d9161e2e475ad0fc5e0df86649984";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(pulldata) {
 
-    console.log(pulldata.name);
-    console.log(pulldata.main.temp);
-    console.log(pulldata.wind.speed);
-    console.log(pulldata.main.humidity);
+    console.log(pulldata.city.name);
+    console.log(pulldata.list[0].main.temp);
+    console.log(pulldata.list[0].wind.speed);
+    console.log(pulldata.list[0].main.humidity);
     
-    var mainCityName = $("<h2>").text(pulldata.name);
-    var tempF = (pulldata.main.temp - 273.15) * 1.80 + 32;
+    var mainCityName = $("<h2>").text(pulldata.city.name);
+    var tempF = (pulldata.list[0].main.temp - 273.15) * 1.80 + 32;
     var mainCityTemp = $("<h4>").text("Current Temperature: " + tempF.toFixed(2) + " " + String.fromCharCode(176) + "F");
-    var mainCityWind = $("<h4>").text("Current Wind Speed: " + pulldata.wind.speed + "MpH");
-    var mainCityHumid = $("<h4>").text("Current Humidity: " + pulldata.main.humidity + "%");
+    var mainCityWind = $("<h4>").text("Current Wind Speed: " + pulldata.list[0].wind.speed + "MpH");
+    var mainCityHumid = $("<h4>").text("Current Humidity: " + pulldata.list[0].main.humidity + "%");
 
     $(".main-city").empty();
     $(".main-city").append(mainCityName,mainCityTemp,mainCityHumid,mainCityWind);
 
-    console.log(pulldata.coord.lon);
-    console.log(pulldata.coord.lat);
-    var mainCityLon = pulldata.coord.lon;
-    var mainCityLat = pulldata.coord.lat;
+    console.log(pulldata.city.coord.lon);
+    console.log(pulldata.city.coord.lat);
+    var mainCityLon = pulldata.city.coord.lon;
+    var mainCityLat = pulldata.city.coord.lat;
     var queryURLAnother = "http://api.openweathermap.org/data/2.5/uvi?appid=1a4d9161e2e475ad0fc5e0df86649984&lat=" + mainCityLat + "&lon=" + mainCityLon;
         $.ajax({
             url: queryURLAnother,
